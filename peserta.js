753354c75d1ecb5c8f98bd0c88cc0fbfec8f7a09
nurihuda <nurih789@gmail.com>
2026-06-09 21:38:00 +0700

    Populate peserta.js with participant data
    Added participant data for LKS GDT Bangka Belitung 2026.

--- peserta.js
+++ peserta.js
@@ -1,4 +1,112 @@
-// Kosongkan sementara isi peserta.js untuk repositori Babel 2026
+// File: peserta.js
+// Data seluruh peserta LKS GDT Bangka Belitung 2026 (Urutan A-Z)
 window.DATA_PESERTA = [
-    // Data peserta Bangka Belitung nanti dimasukkan di sini
+  {
+    "nama": "Ahmad Khadavi",
+    "link": "https://drive.google.com/drive/folders/17XaDG3xP-E6upyzkEi94gLvGMQqX-Dsv?usp=share_link"
+  },
+  {
+    "nama": "Cika Aulia Sari",
+    "link": "https://drive.google.com/drive/folders/1UQp-lqk5-y3WZhepINtbJtentsHRE-cs?usp=share_link"
+  },
+  {
+    "nama": "Davin Apriliano",
+    "link": "https://drive.google.com/drive/folders/1kk1GtqQpJO36FzQxgoac5DEsaWbPfQgX?usp=drive_link"
+  },
+  {
+    "nama": "Delly Nandita",
+    "link": "https://drive.google.com/drive/folders/18sWbkYZmF0vCPyiFdZWpBgjkIcJOGWfO?usp=share_link"
+  },
+  {
+    "nama": "Diana Setiawati",
+    "link": "https://drive.google.com/drive/folders/1X9xDoJOl45cRj8GR67lsh2-CvmzlMGPQ?usp=share_link"
+  },
+  {
+    "nama": "Diva Aulia Saputri",
+    "link": "https://drive.google.com/drive/folders/1TNt1m45gU-WV4_iOoi3_PqaaDoH1WIKt?usp=share_link"
+  },
+  {
+    "nama": "Endi Deriano",
+    "link": "https://drive.google.com/drive/folders/1VATxrJiIefUTaJMmoI07IG3ZXUlx3wsY?usp=sharing"
+  },
+  {
+    "nama": "Fahri Affandi",
+    "link": "https://drive.google.com/drive/folders/1IxDC6IvPqCaY5jvARlTWcaVJ5j41FL0T?usp=share_link"
+  },
+  {
+    "nama": "Ghiani",
+    "link": "https://drive.google.com/drive/folders/1SCI7XZ3GQzIPyNeSBaSzcfJJtEiWyS7v?usp=share_link"
+  },
+  {
+    "nama": "Gilank Ibranajib",
+    "link": "https://drive.google.com/drive/folders/1RdEz9vB-TMa7jNKTvg1O6G9VxakcaK5p?usp=share_link"
+  },
+  {
+    "nama": "Gisella",
+    "link": "https://drive.google.com/drive/folders/1ZBzFPH575Mk0aebnyXpjZiI5rlaxHKCi?usp=share_link"
+  },
+  {
+    "nama": "Herdiyansyah",
+    "link": "https://drive.google.com/drive/folders/1hYaG2OiNQ_vDOB4gBV9OJKk2Wte-HJEJ?usp=share_link"
+  },
+  {
+    "nama": "Icha Restiandani",
+    "link": "https://drive.google.com/drive/folders/1EJPl8VhSadU9-yBKh8jpqxySjpfgsU11?usp=sharing"
+  },
+  {
+    "nama": "Intan",
+    "link": "https://drive.google.com/drive/folders/1mbecJsN6cItPmkzts6jSZk73ecnTqKza?usp=sharing"
+  },
+  {
+    "nama": "Juli Silvani",
+    "link": "https://drive.google.com/drive/folders/1A4_YqtrUCTZMCqGHQaqKeMwglO-J32pl?usp=share_link"
+  },
+  {
+    "nama": "Marvalino",
+    "link": "https://drive.google.com/drive/folders/1abKQFUvVoaQgCUR1OjR_dwvumNan51wg?usp=sharing"
+  },
+  {
+    "nama": "Marvel Emeraldy Pratama",
+    "link": "https://drive.google.com/drive/folders/1U51Id8cG62d51DQ2twCMQJk8L3942Tpc?usp=sharing"
+  },
+  {
+    "nama": "Merry Nurmayanti",
+    "link": "https://drive.google.com/drive/folders/1HTdD5w7JjPsfThZjC2bTx_rnX1ZCXtbD?usp=drive_link"
+  },
+  {
+    "nama": "Natalia",
+    "link": "https://drive.google.com/drive/folders/1Fi2225HcTowPpbwdpEMBgVkclkus2NBl?usp=share_link"
+  },
+  {
+    "nama": "Risky Fadillah",
+    "link": "https://drive.google.com/drive/folders/1OTbwRJdGEu66hv41vK1uJXYGwF6An7ky?usp=sharing"
+  },
+  {
+    "nama": "Septi Fitriani",
+    "link": "https://drive.google.com/drive/folders/1nekJHluESlOFkD20mD8ASoN7zB-2GyTA?usp=share_link"
+  },
+  {
+    "nama": "Valentino Azieraldo",
+    "link": "https://drive.google.com/drive/folders/1wbx0PUFLY1vk3WfgyYerX37j22x3WELc?usp=share_link"
+  },
+  {
+    "nama": "Vellaria Marysa",
+    "link": "https://drive.google.com/drive/folders/1OIsQOskNEHP3uAh3g8GufCEOnhTqHYeK?usp=sharing"
+  },
+  {
+    "nama": "Vika Renita Maharani",
+    "link": "https://drive.google.com/drive/folders/1XoOL1t5phmfjX9r6hmbFB-OQOX-U4FN3?usp=share_link"
+  },
+  {
+    "nama": "Vlorenzya Citra Maytry",
+    "link": "https://drive.google.com/drive/folders/1h9Zs-ymQ_xK0WWJpFBkiva8O_Z4XS8wH?usp=share_link"
+  },
+  {
+    "nama": "Windi",
+    "link": "https://drive.google.com/drive/folders/1_HLrywfoHBny2kSHja4_x3W9jRYwwcAZ?usp=share_link"
+  },
+  {
+    "nama": "Wiwit Herlina",
+    "link": "https://drive.google.com/drive/folders/18Z8S0LUN2hGVdRDY_9HB6R1blh_LVt5O?usp=sharing"
+  }
 ];
